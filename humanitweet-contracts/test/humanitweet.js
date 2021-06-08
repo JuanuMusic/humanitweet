54f295c13ed8859bd7f036c48d727777e7aaa3fb
Juanu <juanu@juanu.com.ar>
2021-06-07 23:33:30 -0300

    Added test for contract to fail on non-humans

--- humanitweet-contracts/test/humanitweet.js
+++ humanitweet-contracts/test/humanitweet.js
@@ -6,8 +6,8 @@ const Humanitweet = artifacts.require("contracts/Humanitweet.sol");
 
 const POH_GOVERNOR = "0x2ad91063e489CC4009DF7feE45C25c8BE684Cf6a";
 const TWEET_URL = "https://ipfs.io/ipfs/QmPS5L36v6zJvzBYuMjDC8ojpx9JNULRG3Perw66qRb6NT?filename=VID_20210322_212500182.mp4"
-contract("Humanitweet", accounts =>  {
-    
+contract("Humanitweet", accounts => {
+
     let humanitweet;
     let poh;
     const HUMAN_ADDRESS = accounts[0];
@@ -18,10 +18,25 @@ contract("Humanitweet", accounts =>  {
         humanitweet = await Humanitweet.new(poh.address);
         await poh.register(HUMAN_ADDRESS);
     })
-    
+
     describe("Humanitweet content creation", () => {
+
+        /* Test human only access */
+        it("Should fail when user not Human tries to publish a humanitweet", async () => {
+            const PREFIX = "VM Exception while processing transaction: ";
+    
+            try {
+                await humanitweet.publishHumanitweet(TWEET_URL, { from: NOT_REGISTERED_ADDRESS });
+            } catch (error) {
+                assert(error, "Expected an error but did not get one");
+                console.log(error.reason);
+                //assert(error.message.startsWith(PREFIX + message), "Expected an error starting with '" + PREFIX + message + "' but got '" + error.message + "' instead");
+            }
+        });
+
+        /* Test NFT mint */
         it("Should correctly mint an NFT and update the tokenCounter", async () => {
-            const receipt = await humanitweet.publishHumanitweet(TWEET_URL, {from: HUMAN_ADDRESS});
+            const receipt = await humanitweet.publishHumanitweet(TWEET_URL, { from: HUMAN_ADDRESS });
             const tokenId = receipt.logs[0].args.tokenId;
             assert(tokenId.eq(new BN(0)), "NFT was not correctly minted");
 
@@ -29,8 +44,9 @@ contract("Humanitweet", accounts =>  {
             assert(tokenCounter.eq(new BN(1)), "Invalid token counter value");
         });
 
+        /* Test minted NFT ownership */ 
         it("Minted NFT should belong to publisher address", async () => {
-            const receipt = await humanitweet.publishHumanitweet(TWEET_URL, {from: HUMAN_ADDRESS});
+            const receipt = await humanitweet.publishHumanitweet(TWEET_URL, { from: HUMAN_ADDRESS });
             const tokenId = receipt.logs[0].args.tokenId;
             assert(tokenId.eq(new BN(1)), "NFT was not correctly minted");
 
@@ -38,8 +54,5 @@ contract("Humanitweet", accounts =>  {
             assert.equal(owner, HUMAN_ADDRESS, "Invalid token owner");
         });
 
-        it("Should fail when user not Human tries to publish a humanitweet", async () => {
-            await truffleAssert.reverts(humanitweet.publishHumanitweet(TWEET_URL, {from: NOT_REGISTERED_ADDRESS}), "User is not registered as human");
-        });
     })
 });
