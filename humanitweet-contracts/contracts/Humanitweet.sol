30ea7ec4573e3d96a7c5349f21da7fe5dc3a91fc
Juanu <juanu@juanu.com.ar>
2021-06-24 22:37:24 -0300

    Distribute UBIs 50% burn 50% creator

--- humanitweet-contracts/contracts/Humanitweet.sol
+++ humanitweet-contracts/contracts/Humanitweet.sol
@@ -5,10 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/accessutils/math/SafeMath.sol";
 import "./IProofOfhumanity.sol";
 
 contract Humanitweet is ERC721, Ownable {
-
+    SafeMath for uint256;
     string HUMAN_NOT_REGISTERED = "HUMAN_NOT_REGISTERED";
     using Strings for uint256;
     address _ubi;
@@ -130,15 +131,27 @@ contract Humanitweet is ERC721, Ownable {
      */
     function support(uint256 tokenId, uint256 ubiAmount) public tokenExists(tokenId) {
 
-        // Burn the UBI on behalf of the caller.
-        ERC20Burnable(_ubi).burnFrom(_msgSender(), ubiAmount);
+        // ammount to burn
+        uint256 toBurn = ubiAmount.div(2);
+        require(toBurn > 0, "Humanitweet: invalid ubi amount to burn");
         
+        // Burn the UBI on behalf of the caller.
+        ERC20Burnable(_ubi).burnFrom(_msgSender(), toBurn);
+        
+        // Transfer remainder to tweet creator
+        uint256 forCreator = ubiAmount.sub(toBurn);
+        if(forCreator > 0) { 
+            ERC20(_ubi).transferFrom(_msgSender, forCreator);
+        }
+
         // Add support based on ubi amount
         _addSupport(tokenId, ubiAmount, _msgSender());
         
     }
 
     function _addSupport(uint256 tokenId, uint256 amount, address supporter) private {
+        
+        uint256 amountToBurn = 
          // Add the amount of support given
         _humanitweets[tokenId].supportGiven += amount;
 
